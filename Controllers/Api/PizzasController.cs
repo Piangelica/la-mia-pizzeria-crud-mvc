e36e3005cbe0961de6a7d07e89f0d6e1d325dedb
Piangelica <101094023+Piangelica@users.noreply.github.com>
2022-05-20 08:18:24 +0200

    Add files via upload

---
+++ Controllers/Api/PizzasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCore_01.Data;
+using NetCore_01.Models;
+
+namespace NetCore_01.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+[ApiController]
+public class PizzasController : ControllerBase
+{
+[HttpGet]
+public IActionResult Get(string? search)
+        {
+            List<Pizze> pizze = new List<Pizze>();
+
+            using (PizzeContext db = new PizzeContext())
+            {
+if (search != null && search != "")
+
+                {
+                   pizze = db.Pizze.Where(pizze => pizze.Nome.Contains(search) || pizze.Descrizione.Contains(search)).ToList<Pizze>();
+                }
+                else
+                {
+                    pizze = db.Pizze.ToList<Pizze>();
+                }
+            }
+
+            return Ok(pizze);
+        }
+    }
+}
+            
+
+
+
+
+                /*
+                pizze = db.Pizze.ToList<Pizze>();
+            }
+            return Ok( pizze);
+        }
+    }*/
+    
+
+
