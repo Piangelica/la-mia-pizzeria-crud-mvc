b7f3c5e1d5133003e72500c9fdd1122f7fbd2b9c
Piangelica <101094023+Piangelica@users.noreply.github.com>
2022-05-20 17:02:16 +0200

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,23 @@
+﻿using NetCore_01.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace NetCore_1.Models
+{
+    public class Category
+{
+        [Key]
+public int Id { get; set; }
+        [Required(ErrorMessage ="Il campo è obbligatorio")]
+        [StringLength(75, ErrorMessage ="il titolo non può superare 75 caratteri")]
+public string  Titolo { get; set; }
+        [JsonIgnore]
+        public List<Pizze> pizzes { get; set; }
+        public Category()
+
+        {
+
+
+        }
+    }
+}
