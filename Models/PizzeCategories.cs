3253d60ad3e1f2b5fe3334579ab9055104d24ed7
Piangelica <101094023+Piangelica@users.noreply.github.com>
2022-05-20 17:06:09 +0200

    Add files via upload

---
+++ Models/PizzeCategories.cs
@@ -0,0 +1,11 @@
+﻿using NetCore_1.Models;
+
+namespace NetCore_01.Models
+{
+    public class PostCategories
+    {
+        public Pizze pizze { get; set; }
+        public List<Category>? Categories { get; set; }
+    }
+}
+
