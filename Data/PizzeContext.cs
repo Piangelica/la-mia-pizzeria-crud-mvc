7bf62efa001b62dd3b20cfd23a5340b3639c13f6
Piangelica <101094023+Piangelica@users.noreply.github.com>
2022-05-21 12:24:31 +0200

    Add files via upload

---
+++ Data/PizzeContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using NetCore_01.Models;
+using NetCore_1.Models;
+
+namespace NetCore_01.Data
+{
+    public class PizzeContext: DbContext
+    {
+        public DbSet<Pizze> Pizze { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=blog_post;Integrated Security=True");
+        }
+    }
+}
+
+       /*public DbSet<Category> Categories { get; set; }
+        public object Category { get; internal set; }
+        public IEnumerable<object> Pizzas { get; internal set; }
+       */
+       
